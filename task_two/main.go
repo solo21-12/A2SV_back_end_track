d074b3bbc4e39aac8d1f5cf9fb1e1c9add2fb571
solo2112 <dawitabrham0021@gmail.com>
2024-08-04 16:13:25 +0300

    task two complted with tests

---
+++ task_two/main.go
@@ -0,0 +1,45 @@
+package tasktwo
+
+import (
+	"strings"
+	"unicode"
+)
+
+func wordFrequencyCount(s string) map[string]int {
+
+	counter := make(map[string]int)
+	words := strings.FieldsFunc(strings.ToLower(s), func(c rune) bool {
+		return !unicode.IsLetter(c) && !unicode.IsNumber(c)
+	})
+
+	for _, word := range words {
+
+		counter[word] += 1
+	}
+
+	return counter
+
+}
+
+func checkPalindrome(s string) bool {
+
+	cur_word := ""
+	for _, word := range s {
+		if unicode.IsLetter(word) || unicode.IsNumber(word) {
+			cur_word += string(word)
+		}
+	}
+
+	left, right := 0, len(s)-1
+
+	for left <= right {
+		if cur_word[left] != cur_word[right] {
+			return false
+		}
+
+		left += 1
+		right -= 1
+	}
+
+	return true
+}
---
+++ task_two/main_test.go
@@ -0,0 +1,24 @@
+package tasktwo
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestWordFrequencyCount(t *testing.T) {
+	word := "abc abc cde"
+	expected := map[string]int{
+		"abc": 2,
+		"cde": 1,
+	}
+	actualResult := wordFrequencyCount(word)
+	assert.Equal(t, expected, actualResult, expected)
+}
+
+func TestCheckPalindrome(t *testing.T) {
+	palindromeWord := "abccba"
+	notPalindromeWord := "abcab"
+	assert.Equal(t, checkPalindrome(palindromeWord), true)
+	assert.Equal(t, checkPalindrome(notPalindromeWord), false)
+
+}
