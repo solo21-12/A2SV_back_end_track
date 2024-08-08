a2aaa38ecfba4eab95bac8ea27dad66ec36f1f6d
solo2112 <dawitabrham0021@gmail.com>
2024-08-08 11:56:24 +0300

    updated(task_six): user model created

--- task_six/model/user_model.go
+++ task_six/model/user_model.go
@@ -1,1 +1,10 @@
 package model
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Email    string             `json:"email" binding:"required"`
+	Password string             `json:"password" binding:"required"`
+	Role     string             `json:"role"`
+}
